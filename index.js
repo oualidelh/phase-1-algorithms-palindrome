c2e854a1a63add6e3e648d25f0aad2d1639d6a1d
oualidelhouari <elhouarioualid@gmail.com>
2023-08-27 23:02:50 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function isPalindrome(word) {
-  //first solution :
+  // 1-first solution :
   let revString = "";
   for (let i = word.length - 1; i >= 0; i--) {
     revString += word[i];
   }
   return revString === word;
-  // second solution :
+  // 2-second solution :
   // let i = 0;
   // let j = word.length - 1;
   // while (word[i] === word[j]) {
@@ -23,10 +23,19 @@ console.log(isPalindrome("mom"));
 */
 
 /*
+1- first solution :
   in this function i first thinked about how to iterate over a string 
   than i thinked that if string is palindrome that means is the reverse 
   of this strinf is equal to the main string than i compared the reverse with 
-  the original String if they are equal it will return true if not it will return false
+  the original String if they are equal it will return true if not it will return false .
+
+2- second solution :
+   in the second solution i tried to iterate over the word from 
+   the first indx to the last index and in the ssame time fro last index to 
+   the first index and compare between them using while if i reach the last index 
+   in first iteration and the fisrt index in second iteration and the letters still 
+   the same i will return true if not i will return false .
+
 */
 
 //You can run `node index.js` to view these console logs
